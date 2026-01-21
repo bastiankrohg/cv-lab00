d1eba560f54cf78843dd825f6cfd28d7265cdd4b
Bastian Krohg <98895859+bastiankrohg@users.noreply.github.com>
2026-01-21 10:57:58 +0100

    First test, brings up camera feed from iPhone

--- py/main.py
+++ py/main.py
@@ -2,11 +2,34 @@ import cv2
 
 
 def lab00():
-    # If you are on Windows, please use: cv2.VideoCapture(..., cv2.CAP_DSHOW)
-    pass
+    device_id = 0
+    cap = cv2.VideoCapture(device_id)
 
+    if not cap.isOpened():
+        print("Error: Could not open camera {device_id}")
+        return
+    else:
+        print(f"Successfully opened camera {device_id}")
+
+    window_title = "Lab 0: Introduction to Python and OpenCV"
+    cv2.namedWindow(window_title, cv2.WINDOW_GUI_NORMAL)
+
+    while True: 
+        success, frame = cap.read()
+
+        if not success:
+            print("The image capture did not succeed. Is the camera OK?")
+            break
+
+        cv2.imshow(window_title, frame)
+        delay_ms = 15
+        key = cv2.waitKey(delay_ms)
+
+        if key >= 0:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     lab00()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
